75eacd769e5f2ee52b348b02572b621833b99587
Dodowingster <aqqil.azman99@gmail.com>
2024-02-28 12:50:17 +0800

    update recent build

--- todolist/todolist/serializers.py
+++ todolist/todolist/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import task
 
-class TaskSerializer(serializers.ModelSerializer):
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class meta:
         model = task
         fields = '__all__'
